55550c1032ac3e240723fe359306d0b2041e1523
Rohyanzidan02 <rohyanzidan02@gmail.com>
2025-03-17 23:16:01 +0700

    Memperbaiki bug pada tampilan Partner.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ import Contact from '../components/Contact';
 import Desain from '../components/Desain';
 import Partner from '../components/Partner';
 import ScrollToTop from '../components/ScrollToTop';
-import IconNavbar from '../icon/Icon';
+
 
 function Home() {
   return (
@@ -74,10 +74,8 @@ function Home() {
         < Partner/>
         {/* <Contact contactSection={contactSection} /> */}
       </div>
-      <IconNavbar/>
       <Contact />
       <Footer />
-      <ScrollToTop />
     </>
   );
 }
